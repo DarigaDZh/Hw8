635bf1f8128a984e62959d8c135aa00db42fc684
DarigaDZh <darigadzh623@gmail.com>
2023-05-02 18:50:44 +0600

    'rez'

--- class1.py
+++ class1.py
@@ -1,3 +1,6 @@
 class Hello:
-    def __init__(self, str):
-        self.str = str
+    def __init__(self, strr):
+        self.strr = strr
+class Hi(Hello):
+    def __init__(self,strr):
+        super().__init__(strr)
--- task.py
+++ task.py
@@ -1,2 +1,5 @@
 from art import tprint
+from class1 import Hi
 tprint("PYTHON")
+aa = Hi("dariga")
+tprint(aa.strr)
